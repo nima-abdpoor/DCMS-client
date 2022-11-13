bddb289154d7a81ac98df93a7ae821ed374b66c1
nima abdpoor <nimaabdpoor@gmail.com>
2022-11-13 18:52:32 +0330

    remote cacheDataSource

--- DCMS/src/main/java/com/nima/dcms/database/datasource/abstract/CacheDataSource.kt
+++
@@ -1,7 +0,0 @@
-package com.nima.dcms.database.datasource.abstract
-
-//import com.nima.dcms.database.entitty.URLIdsEntity
-
-interface CacheDataSourcee {
-    fun getAllURLIds(): List<String>
-}
--- app/src/main/java/com/nima/dcms/MainActivity.kt
+++ app/src/main/java/com/nima/dcms/MainActivity.kt
@@ -1,22 +1,11 @@
 package com.nima.dcms
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-//import com.nima.dcms.database.ObjectBox
-//import com.nima.dcms.database.datasource.implementation.CacheDDImpl
-//import com.nima.dcms.database.entitty.URLIdsEntity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        ObjectBox.init(this)
-//        val dataSource = CacheDDImpl(ObjectBox.store)
-//        ObjectBox.store.boxFor(URLIdsEntity::class.java).put(
-//            URLIdsEntity(0, mutableListOf("1234", "124434"))
-//        )
-//        val results = dataSource.getAllURLIds()
-//        Log.d("TAG", "onCreate: $results")
     }
 }
