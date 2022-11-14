8fa840b922d2bb420b4fdfb71fda2e62a681dd2d
nima abdpoor <nimaabdpoor@gmail.com>
2022-11-14 15:57:42 +0330

    test Dao. everyThing ok

--- app/src/main/java/com/nima/dcms/MainActivity.kt
+++ app/src/main/java/com/nima/dcms/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.nima.dcms
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.nima.dcms.datasource.database.DCMSDataBase
+import com.nima.dcms.datasource.database.entitty.URLIdFirst
+import com.nima.dcms.datasource.database.getDao
+import com.nima.dcms.datasource.database.getDataBase
+import com.nima.dcms.datasource.implementation.MyDaoServiceImpl
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.sql.DatabaseMetaData
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val db = getDao(this)
+        val service = MyDaoServiceImpl(db)
+        CoroutineScope(Dispatchers.IO).launch {
+//            val ids = service.insertURL(URLIdFirst(null, 1254444))
+//            val ids2 = service.insertURL(URLIdFirst(null, 15455))
+//            val ids4 = service.insertURL(URLIdFirst(null, 2544))
+        }
+        CoroutineScope(Dispatchers.IO).launch {
+            val ids = service.getAllUrls()
+            ids.forEach{
+                Log.d("TAG", "onCreate: ${it.urlId}")
+            }
+        }
+
     }
 }
