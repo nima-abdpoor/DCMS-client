417b7938d1515da375d50979d88e2c97246f82c8
nima abdpour <nimaabdpoor@gmail.com>
2022-11-15 20:53:17 +0330

    call our custom netWork manager for activity. for test

--- app/src/main/java/com/nima/dcms/MainActivity.kt
+++ app/src/main/java/com/nima/dcms/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.nima.dcms.datasource.database.getDao
 import com.nima.dcms.datasource.implementation.MyDaoServiceImpl
+import com.nima.network.manager.NetworkManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -15,17 +16,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val db = getDao(this)
         val service = MyDaoServiceImpl(db)
-        CoroutineScope(Dispatchers.IO).launch {
-//            val ids = service.insertURL(URLIdFirst(null, 1254444))
-//            val ids2 = service.insertURL(URLIdFirst(null, 15455))
-//            val ids4 = service.insertURL(URLIdFirst(null, 2544))
-        }
-        CoroutineScope(Dispatchers.IO).launch {
-            val ids = service.getAllUrlFirst()
-            ids.forEach{
-                Log.d("TAG", "onCreate: ${it.urlId}")
-            }
-        }
-
+//        CoroutineScope(Dispatchers.IO).launch {
+////            val ids = service.insertURL(URLIdFirst(null, 1254444))
+////            val ids2 = service.insertURL(URLIdFirst(null, 15455))
+////            val ids4 = service.insertURL(URLIdFirst(null, 2544))
+//        }
+//        CoroutineScope(Dispatchers.IO).launch {
+//            val ids = service.getAllUrlFirst()
+//            ids.forEach{
+//                Log.d("TAG", "onCreate: ${it.urlId}")
+//            }
+//        }
+        val manager = NetworkManager()
+        manager.submitWork(this)
     }
 }
