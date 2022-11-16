5d7dcf136632327b06590af90212d1e7d2d3144c
nima abdpour <nimaabdpoor@gmail.com>
2022-11-16 23:35:43 +0330

    network utils class and models created.

---
+++ Network/src/main/java/com/nima/network/manager/model/NetworkModels.kt
@@ -0,0 +1,5 @@
+package com.nima.network.manager.model
+
+enum class HttpMethods(val value: String){
+    GET("GET"), POST("POST"), PUT("PUT")
+}
---
+++ Network/src/main/java/com/nima/network/manager/util/NetworkUtils.kt
@@ -0,0 +1,4 @@
+package com.nima.network.manager.util
+
+const val READ_TIME_OUT = 10_000
+const val CONNECT_TIME_OUT = 10_000
