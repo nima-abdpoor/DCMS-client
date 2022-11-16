8375aff19559009eafa9884c8d9a176c49b4bff2
nima abdpour <nimaabdpoor@gmail.com>
2022-11-16 23:36:10 +0330

    test methods

--- Network/src/main/java/com/nima/network/manager/UploadDataManager.kt
+++ Network/src/main/java/com/nima/network/manager/UploadDataManager.kt
@@ -1,9 +1,12 @@
 package com.nima.network.manager
 
 import android.content.Context
+import android.os.Bundle
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.nima.network.manager.model.HttpMethods
+import com.nima.network.manager.request.HttpRequestBuilder
 import kotlinx.coroutines.runBlocking
 import java.io.BufferedOutputStream
 import java.io.OutputStream
@@ -14,8 +17,22 @@ import java.net.URL
 class UploadDataManager(appContext: Context, workerParams: WorkerParameters) :
     Worker(appContext, workerParams) {
     override fun doWork(): Result {
-        uploadData()
+        uploadDataTwo()
         return Result.success()
+    }
+
+    private fun uploadDataTwo(){
+        runBlocking {
+            val request = HttpRequestBuilder()
+                .setUrl("http://google.com")
+                .setMethod(HttpMethods.GET)
+                .submit()
+            Log.d("TAG", "uploadDataTwo: " +
+                    "code:${request.responseCode}" +
+                    "message:${request.responseMessage}")
+        }
+
+
     }
 
     private fun uploadData() {
@@ -31,17 +48,17 @@ class UploadDataManager(appContext: Context, workerParams: WorkerParameters) :
                 httpURLConnection.setRequestProperty("Content-Type", "application/json")
                 try {
                     //to tell the connection object that we will be wrting some data on the server and then will fetch the output result
-                    httpURLConnection.doOutput = false
+                    httpURLConnection.doOutput = true
                     // this is used for just in case we don't know about the data size associated with our request
                     httpURLConnection.setChunkedStreamingMode(0)
 
                     // to write tha data in our request
-//                    val outputStream: OutputStream =
-//                        BufferedOutputStream(httpURLConnection.outputStream)
-//                    val outputStreamWriter = OutputStreamWriter(outputStream)
-//                    outputStreamWriter.write(data)
-//                    outputStreamWriter.flush()
-//                    outputStreamWriter.close()
+                    val outputStream: OutputStream =
+                        BufferedOutputStream(httpURLConnection.outputStream)
+                    val outputStreamWriter = OutputStreamWriter(outputStream)
+                    outputStreamWriter.write(data)
+                    outputStreamWriter.flush()
+                    outputStreamWriter.close()
 
                     // to log the response code of your request
                     Log.d(
---
+++ Network/src/main/java/com/nima/network/manager/model/ConfigBody.kt
@@ -0,0 +1,4 @@
+package com.nima.network.manager.model
+
+class ConfigBody {
+}
