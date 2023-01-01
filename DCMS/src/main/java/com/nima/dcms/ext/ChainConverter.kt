5668349cc3ab97fed98526654f078f4d001ec76a
nima-abdpoor <nimaabdpoor@gmail.com>
2023-01-01 23:37:28 +0330

    a function added to convert Headers to json format.

--- DCMS/src/main/java/com/nima/dcms/ext/ChainConverter.kt
+++ DCMS/src/main/java/com/nima/dcms/ext/ChainConverter.kt
@@ -1,17 +1,33 @@
 package com.nima.dcms.ext
 
 import com.nima.common.model.DCMSResponseBody
-import okhttp3.Request
+import okhttp3.Headers
 
-fun Request.getFormattedRequestString(time: String): String {
-    return "{\"request\":\"{\"url\":\"${url()},\"body\":\"${body()}\",\"header\":\"${headers()}\",\"requestTime\":\"${time}\"},"
+fun DCMSResponseBody.getFormattedRequestString(time: String): String {
+    return "\"request\":{\"url\":\"${url}\",\"body\":\"${body}\",\"header\":\"${headers}\",\"requestTime\":\"${time}\"},"
 }
 
 fun DCMSResponseBody.getFormattedResponseString(): String {
-    return "{\"response\":\"{\"body\":\"${body}\",\"header\":\"${headers}\",\"requestTime\":\"${time}\"},"
+    return "\"response\":{\"body\":\"${"body"}\",\"code\":\"${code}\",\"header\":${headers}\"requestTime\":\"${time}\"}"
 }
 
 
-fun String.cleanURL(): String{
-    return if (this[lastIndex] != '/')  "$this/" else this
+fun String.cleanURL(): String {
+    return if (this[lastIndex] != '/') "$this/" else this
+}
+
+fun Headers.toJsonFormat(): String {
+    val result = StringBuilder()
+    var i = 0
+    val size: Int = size()
+    result.append("{")
+    while (i < size) {
+        result.append("\"").append(name(i)).append("\"").append(":\"").append(value(i))
+            .append("\",")
+        i++
+    }
+    val finalHeader = if (size > 0)
+        result.replace(result.length - 2, result.length - 1, "\"}")
+    else result.append("}")
+    return finalHeader.toString()
 }
