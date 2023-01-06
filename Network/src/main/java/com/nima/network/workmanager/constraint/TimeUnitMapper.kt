acf75abfa7359ac3e9b2fb74dc6bec716a8f1b69
nima-abdpoor <nimaabdpoor@gmail.com>
2023-01-06 18:35:08 +0330

    time unit mapper that maps string to TimeUnit

---
+++ Network/src/main/java/com/nima/network/workmanager/constraint/TimeUnitMapper.kt
@@ -0,0 +1,16 @@
+package com.nima.network.workmanager.constraint
+
+import java.util.concurrent.TimeUnit
+
+fun String.mapToTimeUnit(): TimeUnit {
+    return when(this){
+        "0" -> TimeUnit.NANOSECONDS
+        "1" -> TimeUnit.MICROSECONDS
+        "2" -> TimeUnit.MICROSECONDS
+        "3" -> TimeUnit.SECONDS
+        "4" -> TimeUnit.MINUTES
+        "5" -> TimeUnit.HOURS
+        "6" -> TimeUnit.DAYS
+        else -> TimeUnit.DAYS
+    }
+}
