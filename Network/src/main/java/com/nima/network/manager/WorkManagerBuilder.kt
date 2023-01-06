5bb0698880a8df5159904f6c7331ac66f98958f1
nima-abdpoor <nimaabdpoor@gmail.com>
2023-01-06 10:55:43 +0330

    workManager builder class to build OneTime or Periodic work request.

---
+++ Network/src/main/java/com/nima/network/manager/WorkManagerBuilder.kt
@@ -0,0 +1,30 @@
+package com.nima.network.manager
+
+import androidx.work.*
+import java.util.concurrent.TimeUnit
+
+class WorkManagerBuilder {
+
+
+    inline fun <reified T : ListenableWorker> getOneTimeWorkRequest(
+        constraints: Constraints? = null,
+        initialDelay: Pair<Long, TimeUnit>? = null
+    ): OneTimeWorkRequest {
+        val requestBuilder = OneTimeWorkRequestBuilder<T>()
+        constraints?.let { requestBuilder.setConstraints(it) }
+        initialDelay?.let { requestBuilder.setInitialDelay(it.first, it.second) }
+        return requestBuilder.build()
+    }
+
+    inline fun <reified T : ListenableWorker> getPeriodicWorkRequest(
+        repeatInterval: Long,
+        repeatIntervalTimeUnit: TimeUnit,
+        constraints: Constraints? = null,
+        initialDelay: Pair<Long, TimeUnit>? = null
+    ): PeriodicWorkRequest {
+        val requestBuilder = PeriodicWorkRequestBuilder<T>(repeatInterval, repeatIntervalTimeUnit)
+        constraints?.let { requestBuilder.setConstraints(it) }
+        initialDelay?.let { requestBuilder.setInitialDelay(it.first, it.second) }
+        return requestBuilder.build()
+    }
+}
