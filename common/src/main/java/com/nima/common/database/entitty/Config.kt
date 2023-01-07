784881e578c1e69c30e034ff525fb6a2c0884d72
nima-abdpoor <nimaabdpoor@gmail.com>
2023-01-07 22:52:30 +0330

    adding upload policy to Config classes.

--- common/src/main/java/com/nima/common/database/entitty/Config.kt
+++ common/src/main/java/com/nima/common/database/entitty/Config.kt
@@ -3,30 +3,26 @@ package com.nima.common.database.entitty
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.nima.common.model.ConfigBody
+import com.nima.common.utils.DEFAULT_REPEAT_INTERVAL_WORKER_TIME
+import com.nima.common.utils.DEFAULT_REPEAT_INTERVAL_WORK_TIME_UNIT
 
 @Entity
 data class Config(
     @PrimaryKey var id: Long = 0,
     var isLive: Boolean? = false,
-    var syncType: NetWorkSyncType? = NetWorkSyncType.Default,
     var saveRequest: Boolean = false,
     var saveResponse: Boolean = false,
     var saveError: Boolean = true,
     var saveSuccess: Boolean = false,
+    var netWorkType: String? = "6",
+    var repeatInterval: Long = DEFAULT_REPEAT_INTERVAL_WORKER_TIME,
+    var repeatIntervalTimeUnit: String = DEFAULT_REPEAT_INTERVAL_WORK_TIME_UNIT,
+    var requiresBatteryNotLow: Boolean = true,
+    var requiresStorageNotLow: Boolean = true,
+    var requiresCharging: Boolean = false,
+    var requiresDeviceIdl: Boolean = false,
+
 )
-
-enum class NetWorkSyncType(value: String) {
-    Default("0"), Low("1"), Medium("2"), High("3")
-}
-
-fun String.toNetWorkSyncType(): NetWorkSyncType {
-    return when (this) {
-        "1" -> NetWorkSyncType.Low
-        "2" -> NetWorkSyncType.Medium
-        "3" -> NetWorkSyncType.High
-        else -> NetWorkSyncType.Default
-    }
-}
 
 fun ConfigBody?.toConfig(): Config {
     var requestUrls = ""
@@ -37,17 +33,23 @@ fun ConfigBody?.toConfig(): Config {
         return Config(
             id = 0,
             isLive = isLive,
-            syncType = syncType?.toNetWorkSyncType() ?: NetWorkSyncType.Default,
+            netWorkType = netWorkType,
             saveRequest = saveRequest,
             saveResponse = saveResponse,
             saveError = saveError,
             saveSuccess = saveSuccess,
+            repeatInterval = repeatInterval,
+            repeatIntervalTimeUnit = repeatIntervalTimeUnit,
+            requiresBatteryNotLow = requiresBatteryNotLow,
+            requiresStorageNotLow = requiresStorageNotLow,
+            requiresCharging = requiresCharging,
+            requiresDeviceIdl = requiresDeviceIdl,
         )
     } ?: kotlin.run {
         return Config(
             id = 0,
             isLive = false,
-            syncType = NetWorkSyncType.Default
+            netWorkType = "6"
         )
     }
 }
--- common/src/main/java/com/nima/common/model/ConfigBody.kt
+++ common/src/main/java/com/nima/common/model/ConfigBody.kt
@@ -1,5 +1,8 @@
 package com.nima.common.model
 
+import com.nima.common.utils.DEFAULT_REPEAT_INTERVAL_WORKER_TIME
+import com.nima.common.utils.DEFAULT_REPEAT_INTERVAL_WORK_TIME_UNIT
+
 sealed class ResponseClass
 
 data class ConfigBody(
@@ -8,9 +11,15 @@ data class ConfigBody(
     val urlIdFirst: List<UrlIdFirst>? = null,
     val urlIdSecond: List<UrlIdSecond>? = null,
     val isLive: Boolean? = false,
-    var syncType: String? = "0",
     var saveRequest: Boolean = false,
     var saveResponse: Boolean = false,
     var saveError: Boolean = true,
     var saveSuccess: Boolean = true,
+    var netWorkType: String? = "6",
+    var repeatInterval: Long = DEFAULT_REPEAT_INTERVAL_WORKER_TIME,
+    var repeatIntervalTimeUnit: String = DEFAULT_REPEAT_INTERVAL_WORK_TIME_UNIT,
+    var requiresBatteryNotLow: Boolean = true,
+    var requiresStorageNotLow: Boolean = true,
+    var requiresCharging: Boolean = false,
+    var requiresDeviceIdl: Boolean = false,
 ) : ResponseClass()
